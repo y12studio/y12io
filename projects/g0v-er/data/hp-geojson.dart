6f20dbf71b361410bc6b4d7d2b5dae6ec7597bf3
y12studio <y12studio@gmail.com>
2014-08-12 12:53:30 +0800

    update hp.geojson

--- projects/g0v-er/data/hp-geojson.dart
+++ projects/g0v-er/data/hp-geojson.dart
@@ -15,6 +15,7 @@ String getPrettyJSONString(jsonObject) {
   return encoder.convert(jsonObject);
 }
 /**
+ * ref https://raw.githubusercontent.com/sfsheath/roman-amphitheaters/master/roman-amphitheaters.geojson
 geometry: {
  type: "Point",
  coordinates: [40.728926,34.749855]
@@ -24,20 +25,30 @@ void main() {
 
   Future<String> finishedReading = new File("hp-geo-raw1.json").readAsString(encoding: UTF8).then((text) {
     Map rMap = JSON.decode(text);
+    List features = rMap['features'];
     rMap['hospitals'].forEach((k, v) {
       if (v.runtimeType.toString().contains('Map')) {
         Map m = v;
         if (m.containsKey('lon')) {
           Map geometry = new Map();
           List coordinates = [double.parse(m['lon']), double.parse(m['lat'])];
-          m['geometry'] = geometry;
           geometry['coordinates'] = coordinates;
           geometry['type'] = 'Point';
-          m.remove('lon');
-          m.remove('lat');
+          
+          Map feature = new Map();
+          Map properties = new Map();
+          feature['type']= "Feature";
+          feature['id'] = k;
+          feature['properties']=properties;
+          properties['title']=v['name'];
+          properties['address']=v['addr'];
+          properties['abbr']=v['abbr'];
+          feature['geometry'] = geometry;
+          features.add(feature);
         }
       }
     });
+    rMap.remove("hospitals");
     String rj = getPrettyJSONString(rMap);
     print(rj);
     new File("hp.geojson").writeAsString(rj,encoding: UTF8);
