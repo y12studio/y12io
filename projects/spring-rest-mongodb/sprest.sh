02bcc2def00ae1573fc7f9085606bc506bbaa446
y12studio <y12studio@gmail.com>
2014-08-21 17:16:13 +0800

    fixed gradle version issue #32

--- projects/spring-rest-mongodb/sprest.sh
+++ projects/spring-rest-mongodb/sprest.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd /opt/gs-accessing-mongodb-data-rest/complete
-java -jar build/libs/gs-accessing-mongodb-data-rest-0.1.0.jar \
+cd /opt/sprest/build/libs
+java -jar sprest-0.1.0.jar \
   --spring.data.mongodb.uri=mongodb://$DB_1_PORT_27017_TCP_ADDR:$DB_1_PORT_27017_TCP_PORT/mydb
